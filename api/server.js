27ef0534728ba5fb09d3d9d7a271bf66d1b75dc4
qsysmine <echomanari@gmail.com>
2015-06-01 15:00:45 +0000

    .

--- api/server.js
+++ api/server.js
@@ -1,3 +1,4 @@
+v = "0";
 var api = require("express")();
 var Firebase = require("firebase");
 var fb = new Firebase("https://qpqi.firebaseio.com/");
@@ -11,12 +12,39 @@ api.get("/i/:p", function(req, res) {
         var b64 = _s.split("base64,")[1];
         var buff = new Buffer(b64, "base64");
         res.writeHead(200, {
-            'Content-type':type
+            'Content-type': type
         });
         res.end(buff);
     }, function(e) {
         res.writeHead(500);
         res.end(e);
     });
+});
+api.get("/i", function(req, res) {
+    res.writeHead(500, {
+        'Content-type': "text/json"
+    });
+    res.end(JSON.stringify({
+        api: {
+            name: "quickImage API v" + v,
+            url: "a.qi.qzqp.tk",
+            owner: "Quartz Systems",
+            reportBugs: "aris@qsysmine.tk"
+        },
+        message: "Use /i/[image ID] to request an image."
+    }));
+});
+api.get("/", function(req, res) {
+    res.writeHead(200, {
+        'Content-type': "text/json"
+    });
+    res.end(JSON.stringify({
+        api: {
+            name: "quickImage API v" + v,
+            url: "a.qi.qzqp.tk",
+            owner: "Quartz Systems",
+            reportBugs: "aris@qsysmine.tk"
+        }
+    }));
 });
 api.listen(process.env.PORT || 3000);
